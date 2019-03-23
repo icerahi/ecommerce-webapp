6bdd1ac0e7a61c31bddf752f3e55be84a05f8a6c
icecream <zanjarwhite@gmail.com>
2019-03-24 02:51:07 +0600

    uploaded

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-
+from .forms import ContactForm
 
 def home(request):
-    return render(request,'home.html')
+
+    context={'title':'home page'}
+    return render(request,'home.html',context)
+
+
+def about(request):
+    context={'title':'about page'}
+    return render(request,'home.html',context)
+
+
+def contact(request):
+    contact_form=ContactForm(request.POST or None)
+
+    context={'title':'contact page',
+             'form':contact_form}
+
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    return render(request,'home.html',context)
